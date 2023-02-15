cb8d73a222018a5efe81af623699e09ca108a809
kenlee <larukuhyde05@gmail.com>
2023-02-15 23:43:20 +0800

    deploy

---
+++ assets/AdminHomeView-9624bf21.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t}from"./index-75a156c2.js";const c={};function n(r,s,a,_,p,m){return o(),t("h1",null,"Home")}const i=e(c,[["render",n]]);export{i as default};
---
+++ assets/AdminOrderListView-22b31eb8.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-75a156c2.js";const n={};function t(o,s){return c(),r("h1",null,"訂單列表")}const a=e(n,[["render",t]]);export{a as default};
